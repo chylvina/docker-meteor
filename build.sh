b20e6b4c342f317406ae9cfc6b4fbe0e816993f9
xiongliang.xl <xiongliang.xl@alibaba-inc.com>
2016-05-21 23:18:59 +0800

    test

--- build.sh
+++ build.sh
@@ -5,6 +5,11 @@ set -e
 apt-get update -y
 apt-get install -y curl bzip2 build-essential python git
 
+# https://github.com/meteor/meteor/issues/4019
+apt-get install locales
+locale-gen en_US.UTF-8
+localedef -i en_GB -f UTF-8 en_US.UTF-8
+
 # install node
 NODE_VERSION=0.10.43
 NODE_ARCH=x64
@@ -24,8 +29,3 @@ ln -sf /opt/nodejs/bin/npm /usr/bin/npm
 
 # install meteor
 curl -sL https://install.meteor.com | sed s/--progress-bar/-sL/g | /bin/sh
-
-# https://github.com/meteor/meteor/issues/4019
-apt-get install locales
-locale-gen en_US.UTF-8
-localedef -i en_GB -f UTF-8 en_US.UTF-8
